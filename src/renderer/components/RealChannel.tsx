5354d9235f01c89c805c3c7615b98b477bcc7ad3
Pedro Henrique Kopper <pedro.kopper@balena.io>
2022-06-13 20:02:01 -0300

    Refactor RealChannel as a component class

--- src/renderer/components/RealChannel.tsx
+++ src/renderer/components/RealChannel.tsx
@@ -1,23 +1,54 @@
 import PropTypes from 'prop-types';
 import './RealChannel.css';
 
-const RealChannel = (props: any) => {
-  const { name, min, max, value, unit } = props;
-  const offset = (max - min) / 2;
-  return (
-    <div className="channelParent">
-      <div className="channelName">{name}</div>
-      <progress
-        className="progressBar"
-        value={value + offset}
-        max={max - min}
-      />
-      <div className="channelValue">
-        {value.toFixed(4)} {unit}
+const React = require('react');
+
+class RealChannel extends React.Component {
+  constructor(props: any) {
+    super(props);
+
+    this.state = { threshold: { min: 0, max: 0 } };
+  }
+
+  render() {
+    const { name, min, max, value, unit } = this.props;
+    const { threshold } = this.state;
+
+    const offset = (max - min) / 2;
+    return (
+      <div className="channelParent">
+        <div className="channelName">{name}</div>
+        <div>
+          <input
+            type="range"
+            min={min}
+            max={0}
+            className="channelThreshold"
+            // value={threshold.max}
+            defaultValue={min / 2}
+          />
+          <input
+            type="range"
+            min={0}
+            max={max}
+            className="channelThreshold"
+            // value={threshold.max}
+            defaultValue={max / 2}
+            style={{ direction: 'rtl' }}
+          />
+        </div>
+        <progress
+          className="progressBar"
+          value={value + offset}
+          max={max - min}
+        />
+        <div className="channelValue">
+          {value.toFixed(4)} {unit}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 RealChannel.propTypes = {
   name: PropTypes.string.isRequired,
